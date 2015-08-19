2a76182055cef3be03ca53988f1e8730f7b458a5
Hung Nguyen <hunnguyen@atlassian.com>
2015-08-19 22:58:18 +0700

    TipCalculator: Init project

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Nguyen Duy Hung on 8/11/15.
+//  Copyright (c) 2015 Nguyen Duy Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercentage: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        billField.becomeFirstResponder()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercent = tipPercentages[tipPercentage.selectedSegmentIndex]
+        
+        
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * tipPercent
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
+
